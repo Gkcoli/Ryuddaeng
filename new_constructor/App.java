6cc4b6be6e92e8e55a93f037d15f0a7508d14ab0
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2022-08-25 15:49:59 +0800

    Create App.java

---
+++ new_constructor/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Creates a new object named "Knight" from Character class
+        Character knight = new Character(10, 5, 0, "Chen");
+        // Calls the sayMyName method
+      
+
+        // Creates a new object named "mage" from Character class
+        Character mage = new Character(0, 5, 10, "Akasha");
+        // Calls the sayMyName method
+       
+
+        // Creates a new object named "thief" from Character class
+        Character thief = new Character(5, 10, 0, "Gondar");
+        // Calls the sayMyName method
+        
+
+        // Creates a new object named "archer" from Character class
+        Character archer = new Character(12 , 10, 3, "Kate");
+        // Calls the sayMyName method
+      
+
+         // Creates a new object named "priest" from Character class
+        Character priest = new Character(1, 3, 5, "Jake");
+        // Calls the sayMyName method
+    }
+}
