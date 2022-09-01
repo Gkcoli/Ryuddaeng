a8783ec97a0309f9259ad20a26a1ac622fa4d1d7
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2022-09-01 15:53:46 +0800

    Create Cat.java

---
+++ Class_pet/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+
+    public static void main(String[] args){
+        
+        Pet Gwen = new Pet("Gwen", 15, 60, true);
+        
+        Pet Nicole = new Pet("Nicole", 99, 60, true);
+
+        Pet Felicia = new Pet("Felicia", 75, 60, false);
+        
+    }
+}
+
+
+ 
