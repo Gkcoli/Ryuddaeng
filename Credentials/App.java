bc7932bc31c2d697bab87498cb11a5da7aaa80d6
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2022-09-08 14:38:10 +0800

    Create App.java

---
+++ Credentials/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     
+    Student Coli = new Student("Colinares ", "Gian Karl ", "L. ", "12/10/03", true);
+    
+    Coli.studentNumber = "2022103241";
+    Coli.sayMyStudentNumber();
+    
+    Coli.studentEmailAddress = "colinaresgl@students.national-u.edu.ph";
+    Coli.sayMyStudentEmailAdress();
+
+    Coli.amIAwesome = "yeah im Awesome! ";
+    Coli.sayamIAwesome();
+}
+
+
+
+
+
+
+}
