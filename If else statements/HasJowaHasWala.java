4335bd330b3669fd2cb6c86301aacc3d54de0e6d
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2022-10-03 19:15:48 +0800

    Create HasJowaHasWala.java

---
+++ If else statements/HasJowaHasWala.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner s = new Scanner(System.in);
+
+        boolean hasJowa = false;
+        boolean hasWala = false;
+     
+        if(hasJowa || hasWala) System.out.println("Edi kayo na meron");
+        else System.out.println("Bawi nalang nextlife pre");
+    }
+}
