29e33fb0e89ed2032737a99106a6f913e4e90023
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2022-10-20 15:22:35 +0800

    Create App.java

---
+++ Clock nested for loops/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+      for (int hours = 0; hours < 12; hours++){
+
+        for (int minutes = 0; minutes < 60; minutes++){
+            for (int sec = 0; sec < 60; sec++){
+                System.out.println(hours+ ":" + minutes+ ":"+ sec+ ":");
+        }
+
+
+      
+    }
+
+
+
+        
+    }
+  }  
+}
