a58da09c83cf9a89800d72c88b80a07e2f3c5a59
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2022-10-20 15:08:15 +0800

    Create Multiarray.java

---
+++ Array/Multiarray.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        char[][] Letters = {
+                { 'A', 'B', 'C' },
+                { 'D', 'E', 'F' },
+                { 'G', 'H', 'I'},
+                
+        };
+
+        // Access element that is selected
+        System.out.println(Letters[1][1]);
+        System.out.println(Letters[2][0]);
+        System.out.println(Letters[0][1]);
