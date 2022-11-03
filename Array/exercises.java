fca0b93227bc3b2bf4782da9ae9457afeeab6fce
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2022-11-03 16:38:29 +0800

    Create exercises.java

---
+++ Array/exercises.java
@@ -0,0 +1,189 @@
+public class exercise {
+    public static void main(String[] args) throws Exception {
+  
+        char [][] mdArray = {{'S','L','A','M'}, {'T','I','L', 'E'}, {'E','A','T', 'S'}, {'P', 'R', 'O', 'S'}};
+    
+  
+        // WORD 1
+        System.out.println("1. WORD 1");
+        
+        for (int row = 0; row < 4;  row++) {
+            System.out.println("");
+            for (int column = 0; column < 4; column++){
+                if (row == 0){
+                    System.out.print(mdArray[row][column]);
+                }
+            }
+
+        }
+        // WORD 2
+        System.out.print("2. WORD 2");
+
+        for (int row = 0; row < 4; row++) {
+            System.out.println("");
+            for (int column = 0; column < 4; column++) {
+                if (row == 1) {
+                    System.out.print(mdArray[row][column]);
+                }
+            }
+        }
+
+    
+       
+        
+        // WORD 3
+        System.out.print("3. WORD 3");
+
+        for (int row = 0; row < 4; row++){
+            System.out.println("");
+            for (int column = 0; column < 4; column++){
+                if (row == 2){
+                    System.out.print(mdArray[row][column]);
+                }
+            }
+        }
+        
+        // WORD 4
+        System.out.println("4. WORD 4");
+
+        for (int row = 0; row < 4; row++){
+            System.out.println("");
+            for(int column = 0; column < 4; column++){
+                if (row == 3){
+                    System.out.print(mdArray[row][column]);
+                }
+            }
+        }
+        
+       
+        // WORD 5
+        System.out.print("");
+        System.out.println(" 5. WORD 5");
+        for (int row = 0; row < 4; row++){
+            for(int column = 0; column < 4; column++){
+                if (column== 0){
+                    System.out.println(mdArray[row][column]);
+                }
+            }
+
+        }
+        
+
+        
+        // WORD 6
+        System.out.println("");
+        System.out.println("6. WORD 6");
+        for (int row = 0; row < 4; row++){
+            System.out.print("");
+            for(int column =0; column < 4; column++){
+                if (column== 1){
+                    System.out.println(mdArray[row][column]);
+                }
+            }
+        }
+
+        
+        // WORD 7
+        System.out.println("");
+        System.out.println("7. WORD 7");
+        for (int row = 0; row < 4; row++){
+            System.out.print("");
+            for(int column =0; column < 4; column++){
+                if (column== 2){
+                    System.out.println(mdArray[row][column]);
+                }
+            }
+        }
+        // WORD 8
+        System.out.println("");
+        System.out.println("8. WORD 8");
+        for (int row = 0; row < 4; row++){
+            System.out.print("");
+            for(int column =0; column < 4; column++){
+                if (column== 3){
+                    System.out.println(mdArray[row][column]);
+                }
+            }
+        }
+        // WORD 9
+        System.out.println("");
+        System.out.println("9. WORD 9");
+        for (int row =3; row >= 0; row--){
+            System.out.print("");
+            for(int column =3; column >= 0; column--){
+                if(column== 0){
+                    System.out.println(mdArray[row][column]);
+                }
+            }
+        }
+        // WORD 10
+        System.out.println("");
+        System.out.println("10. WORD 10");
+        for (int row =3; row >= 0; row--){
+            System.out.print("");
+            for(int column =1; column >=0; column--){
+                if(column== 1 ){
+                    System.out.println(mdArray[row][column]);
+                }
+            }
+        }
+        // WORD 11
+        System.out.println("");
+        System.out.println("11. WORD 11");
+        for (int row =0; row <=3; row++){
+            System.out.print("");
+            for(int column =0; column <=3; column++){
+                if(column== 2 && row ==0 || row== 1 && column == 2 || row ==2 && column== 2){
+                    System.out.println(mdArray[row][column]);
+                }
+            }
+
+        }
+        // WORD 12
+        System.out.println("");
+        System.out.println("12. WORD 12");
+        for (int row =0; row <=3; row++){
+            System.out.print("");
+            for(int column=0; column <=3; column++){
+                if(row == 3 && column == 0 || row ==3 && column ==1 || row ==3 && column ==2){
+                    System.out.println(mdArray[row][column]);
+                }
+            }
+
+        }
+        // WORD 13
+        System.out.println("");
+        System.out.println("13. WORD 13");
+        for (int row =3; row >= 0; row--){
+            System.out.print("");
+            for(int column =3; column >= 0; column--){
+                if(row == 3 && column == 0 || row == 2 && column == 0 || row ==1 && column ==0){
+                    System.out.println(mdArray[row][column]);
+                }
+            }
+        }
+        // WORD 14
+        System.out.println("");
+        System.out.println("14. WORD 14");
+        for (int row =0; row <=3; row++){
+            System.out.print("");
+            for(int column =0; column <= 3; column++){
+                if(row == 2 && column != 3){
+                    System.out.print(mdArray[row][column]);
+                }
+    }   
+  }
+        // WORD 15
+        System.out.println("");
+        System.out.println("15. WORD 15"); 
+        for (int row =0; row <=3; row++){
+            System.out.print("");
+            for(int column =0; column <= 3; column++){
+                if(row == 1 && column == 0 || row ==2 && column ==1 || row ==3 && column ==2){
+                    System.out.print(mdArray[row][column]);
+                }
+            }
+        } 
+}
+}
+
