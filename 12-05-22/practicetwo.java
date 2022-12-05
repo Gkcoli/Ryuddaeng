7296f584f98a5d2a7736052666f1633911c373d3
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2022-12-05 12:17:23 +0800

    Create practicetwo.java

---
+++ 12-05-22/practicetwo.java
@@ -0,0 +1,12 @@
+import java.lang.Math.*;
+class App
+{
+	public static void main(String arg[])	
+	{
+            int x1,x2,y1,y2;
+	    double dis;
+	    x1=1;y1=7;x2=5;y2=9;
+	    dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
+            System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+	}
+}
